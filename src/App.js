df383cc50700aa22eba0fae405ec67fd80e72e4c
Rozalia Ravnachka <rozalia.ravnachka2002@gmail.com>
2024-04-11 18:44:46 +0300

    add app components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import CreatePage from "./components/CreatePage/CreatePage";
+import Dashboard from "./components/Dashboard/Dashboard";
+import DetailsPage from "./components/DetailsPage/DetailsPage";
+import EditPage from "./components/EditPage/EditPage";
+import LoginPage from "./components/LoginPage/LoginPage";
+import RegisterPage from "./components/RegisterPage/RegisterPage";
+import MyPetsPage from "./components/MyPetsPage/MyPetsPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div id="container">
+      <Header />
+      {/* <!-- Main Content --> */}
+      <main id="site-content"></main>
+
+      {/* <!-- Login Page ( Only for Guest users ) --> */}
+
+      {/* <!-- Register Page ( Only for Guest users ) --> */}
+
+      {/* <!-- Dashboard Page ( for Guests and Users ) --> */}
+
+      {/* <!-- Details Page ( for Guests and Users ) --> */}
+
+      {/* <!-- Create Page ( Only for logged-in users ) --> */}
+
+      {/* <!-- Edit Page ( Only for the creator )--> */}
+
+      {/* <!-- My Pets Page ( Only for logged-in users ) --> */}
+
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
