b47dec89829c170ae79cff24190d2d61fc18ab2a
Rozalia Ravnachka <rozalia.ravnachka2002@gmail.com>
2024-04-11 19:21:23 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import CreatePage from "./components/CreatePage/CreatePage";
@@ -14,21 +14,17 @@ function App() {
     <div id="container">
       <Header />
       {/* <!-- Main Content --> */}
-      <main id="site-content"></main>
-
-      {/* <!-- Login Page ( Only for Guest users ) --> */}
-
-      {/* <!-- Register Page ( Only for Guest users ) --> */}
-
-      {/* <!-- Dashboard Page ( for Guests and Users ) --> */}
-
-      {/* <!-- Details Page ( for Guests and Users ) --> */}
-
-      {/* <!-- Create Page ( Only for logged-in users ) --> */}
-
-      {/* <!-- Edit Page ( Only for the creator )--> */}
-
-      {/* <!-- My Pets Page ( Only for logged-in users ) --> */}
+      <main id="site-content">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/create-page" element={<CreatePage />} />
+          <Route path="/edit-page" element={<EditPage />} />
+          <Route path="/my-pets" element={<MyPetsPage />} />
+          <Route path="/details" element={<DetailsPage />} />
+        </Routes>
+      </main>
 
       <Footer />
     </div>
